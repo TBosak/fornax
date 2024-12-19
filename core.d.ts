7c650ff69533571e60fba502be16646256a72baa
Tim B <41713011+TBosak@users.noreply.github.com>
2024-12-18 21:39:14 -0600

    added shims for html & css imports - not working

--- core.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.css' {
-    const styles: {
-      readonly [key: string]: string
-    }
-    export default styles
-  }
---
+++ core/types/global.d.ts
@@ -0,0 +1,9 @@
+export declare module "*.html" {
+  const content: string;
+  export default content;
+}
+
+export declare module "*.css" {
+  const content: string;
+  export default content;
+}
